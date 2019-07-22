92ea62d07c310d387279b28bc8618174e76dfbd6
federico.michell <WHNZxiJ4DVyv>
2019-07-22 10:54:00 -0600

    Creando repo de prueba

---
+++ src/GestorDB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Federico
+ * Date: 7/22/2019
+ * Time: 10:52 AM
+ */
+
+namespace Vigoron\GestorDatos;
+
+
+class GestorDB
+{
+
+}
---
+++ src/Sql.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Federico
+ * Date: 7/22/2019
+ * Time: 10:47 AM
+ */
+
+namespace Vigoron\GestorDatos;
+
+
+class Sql
+{
+
+}
